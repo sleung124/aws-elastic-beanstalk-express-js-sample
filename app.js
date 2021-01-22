b3851a67b44af62b5f57b7e7c5da5adbbd75feac
Sleung124 <samuelleung124@gmail.com>
2021-01-22 15:09:05 -0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,32 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('I love notepad++!'));
+app.get('/', (req, res) => res.send('Here is a list of the restricted Legendary Pokémon that will be available:
+
+Mewtwo
+Lugia
+Ho-Oh
+Kyogre
+Groudon
+Rayquaza
+Dialga
+Palkia
+Giratina
+Reshiram
+Zekrom
+Kyurem
+Xerneas
+Yveltal
+Zygarde
+Cosmog
+Cosmoem
+Solgaleo
+Lunala
+Necrozma
+Zacian
+Zamazenta
+Eternatus
+Calyrex (fused with Glastrier or Spectrier)'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
